9d26e56e6913cc371299d280bee2099fe4880ff9
Vladyslav Malovanyi <sky0nf1re.bc@icloud.com>
2023-09-21 20:23:30 +0200

    dev.hw10.HTTP

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,6 @@
 import http.HttpImageStatusCli;
 
 public class App {
-
     public static void main(String[] args) {
         HttpImageStatusCli cli = new HttpImageStatusCli();
         cli.askStatus();
